cb056d292984be11c1b19eaafde7dd5c3f62fe1b
Rawle Juglal <rawledjuglal@gmail.com>
2023-04-15 18:47:55 -0500

    bootstrapped
    Bootstrapped Home & About Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route, Link } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
 import './index.css'
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Layout />}>
+    <Route path="/about" element={<About />}/>
+    <Route index element={<Home />}/>
+  </Route>
+))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
