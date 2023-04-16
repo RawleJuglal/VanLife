4f62fc9288d8a943a0a93945e23036394a6bf6bf
Rawle Juglal <rawledjuglal@gmail.com>
2023-04-15 19:54:26 -0500

    completed intro
    Finished intro section

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,10 +1,33 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { useLoaderData, Link } from "react-router-dom"
+import { getVans } from "../../hooks/api"
 
-export default function Home() {
+export function loader(){
+    return getVans()
+}
+
+export function Vans() {
+    const vans = useLoaderData()
+
+    const vanElements = vans.map(van => (
+        <div key={van.id} className="van-tile">
+            <Link to={`/vans/${van.id}`}>
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
+        </div>
+    ))
+
     return (
-        <div className="home-container">
-            <h1>Vans Go Here</h1>
+        <div className="van-list-container">
+            <h1>Explore our van options</h1>
+            <div className="van-list">
+                {vanElements}
+            </div>
         </div>
     )
-};
+}
