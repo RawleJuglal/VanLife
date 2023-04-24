2b143629ceb6bffd986f90ae205e15b7611be6fe
Rawle Juglal <RawleJuglal@users.noreply.github.com>
2023-04-24 17:13:52 -0500

    redirect bug
    issue with redirecting to host

--- src/pages/Vans/VanDetails.jsx
+++ src/pages/Vans/VanDetails.jsx
@@ -1,34 +1,45 @@
-import React,{ useState, useEffect} from "react"
-import { Link, useParams, useLocation, useLoaderData } from "react-router-dom"
+import React,{ useState, useEffect, Suspense} from "react"
+import { Link, useParams, useLocation, useLoaderData, defer, Await } from "react-router-dom"
 import { getVans } from "../../hooks/api"
 
 export function loader(params) {
-    return getVans(params.id)
+    return defer({vans: getVans(params.id)})
 }
 
 export function VanDetail() {
     // const params = useParams()
     const location =useLocation()
-    const van = useLoaderData()
+    const dataPromise = useLoaderData()
 
     const search = location.state?.search || ''
     const type = location.state?.type || 'all'
 
     return(
         <div className="van-detail-container">
-            <Link
-                to={`..${search}`}
-                relative="path"
-                className="back-button"
-            >&larr; <span>Back to {type} vans</span></Link>
-                <div className="van-detail">
-                    <img src={van.imageUrl} />
-                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
-                    <h2>{van.name}</h2>
-                    <p className="van-price"><span>${van.price}</span>/day</p>
-                    <p>{van.description}</p>
-                    <button className="link-button">Rent this van</button>
-                </div>
+            <Suspense fallback={<h1>Loading Van...</h1>}>
+                <Await resolve={dataPromise.vans}>
+                    { van =>{
+                        return(
+                            <>
+                            <Link
+                                to={`..${search}`}
+                                relative="path"
+                                className="back-button"
+                            >&larr; <span>Back to {type} vans</span></Link>
+                            <div className="van-detail">
+                                <img src={van.imageUrl} />
+                                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                                <h2>{van.name}</h2>
+                                <p className="van-price"><span>${van.price}</span>/day</p>
+                                <p>{van.description}</p>
+                                <button className="link-button">Rent this van</button>
+                            </div>
+                            </>
+                        ) 
+                    }}
+                </Await>
+            </Suspense>
+            
         </div>
     )
 }
